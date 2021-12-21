8b1d7e2dd22d991c9cd32fd1363623f1eb2ac6cf
Regislaine Regis <nanafidelis@gmail.com>
2021-12-21 11:29:39 -0300

    realizaçao requisito 4

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 let palette = document.querySelector('#color-palette');
+let pixel = document.querySelector('#pixel-board');
 
 const corPaleta = ['black', 'yellow', 'green', 'red'];
 
 for (let index = 0; index < corPaleta.length; index += 1){
-    // criando 4 divs
-    let newDiv = document.createElement('div');
-    newDiv.className = 'color';  
-    palette.appendChild(newDiv);  
+    // criando 4 divs da paleta de cores
+    palette.appendChild(criarElemento('div', 'color')); 
+
   // stylizando as 4 paletas
-    if(corPaleta[index] !== 'white'){
-        let corDiv = document.getElementsByClassName('color')[index];
-         corDiv.style.background = corPaleta[index];
+    mudarCorPaleta(corPaleta,index);
+}
+// função para criar elementos(divs, p, li, ul, etc) com os nomes de suas classes
+function criarElemento(elemento, classe){
+    let newElemento = document.createElement(elemento);
+    newElemento.className = classe;  
+    return newElemento;   
+}
+//função que styliza a cor de fundo da paleta de 4 cores e sua borda
+function mudarCorPaleta(arrayPaleta, indexPaleta){
+    if(arrayPaleta[indexPaleta] !== 'white'){
+        let corDiv = document.getElementsByClassName('color')[indexPaleta];
+         corDiv.style.background = arrayPaleta[indexPaleta];
          corDiv.style.border = 'solid 1px black';
         }
 }
-function criarDiv(){
-       
+// criando as divs de 25pixel, sendo 5 elementos de largura e 5 elementos de comprimento
+for(indexLinha = 0; indexLinha < 5; indexLinha += 1){    
+    for(indexColuna = 0; indexColuna < 5; indexColuna += 1){
+     pixel.appendChild(criarElemento('div', 'pixel'));     
+    }
+    //criando elemento apos 5 div's, pula a linha
+    pixel.appendChild(criarElemento('br','pulalinha'));
 }
-function MudarCorPaleta(){
-
+// pegando a qtidade de divs criadas no quadro pixel e armazenando na variavel tamanho
+let tamanho = document.getElementsByClassName('pixel').length;
+console.log(tamanho);
+//for que styliza a cor de fundo do pixel de 25 cores e sua borda
+for(let index = 0; index < tamanho; index += 1){
+    let corPixel = document.getElementsByClassName('pixel')[index];
+        corPixel.style.background = 'white';
+        corPixel.style.border = 'solid 1px black';
+        if (index % 5 === 0){
+        corPixel.style.margin = 0;
+        }
 }
-
